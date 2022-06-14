183e16900b3d7e832cc09db1ba8df73b0b5f4993
Sapna <sapnasawai1999@gmail.com>
2022-06-14 22:54:13 +0530

    Todo List

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Footer = () => { 
+    return (
+        <footer className="bg-dark text-light py-3 fixed-bottom">
+            <p className="text-center">
+            Copyright &copy; MyTodosList.com
+            </p>
+        </footer>
+    )
+}
---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {TodoItem} from "./TodoItem";
+
+export const Todos = (props) => {
+    let myStyle = {
+        minHeight: "70vh",
+        margin: "40px auto"
+    }
+    return (
+        <div className="container" style={myStyle}>
+            <h3 className="my-4 text-success">Todos List</h3>
+            {props.todos.length===0? "No Tasks to display":  
+            props.todos.map((todo)=>{
+                console.log(todo.sno);
+                return (<TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>   
+                )
+            })
+              } 
+        </div>
+    )
+}
